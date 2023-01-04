f5b7acb087bf6df9e6a36cf3e043362efe38fa34
Asoronite <118924223+Asoronite@users.noreply.github.com>
2023-01-04 17:46:23 +0100

    Delete Position-Example directory

--- Position-Example/index.js
+++
@@ -1,9 +0,0 @@
-// const revers = ("Clarusway Rocks!")
-
-// for (let i=(revers.length -1) ; i>=0; i--){
-//     console.log(revers[i]);
-// }
-
-const names = ["emir","oguzhan","levent","tarik"]
-console.log(names);
-console.log(names.length);
